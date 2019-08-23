a071ab8ff7a100d4eca2d71558badcf8c918c3ee
rusrom <rusrom@guyfawkes.33mail.com>
2019-08-23 20:39:23 +0300

    Create superuser and correct mistake in user profile manager: self._db instead of self.__db

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -15,7 +15,7 @@ class UserProfileManager(BaseUserManager):
         user = self.model(email=email, name=name)
 
         user.set_password(password)
-        user.save(using=self.__db)
+        user.save(using=self._db)
 
         return user
 
@@ -24,7 +24,7 @@ class UserProfileManager(BaseUserManager):
         user = self.create_user(email, name, password)
         user.is_superuser = True
         user.is_staff = True
-        user.save(using=self.__db)
+        user.save(using=self._db)
 
         return user
 
