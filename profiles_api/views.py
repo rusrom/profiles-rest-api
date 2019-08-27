b69a237c78e1775c954f9640e136d81a85216438
rusrom <rusrom@guyfawkes.33mail.com>
2019-08-27 16:38:44 +0300

    Added profiles viewset

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,8 +2,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
 
 from profiles_api import serializers
+from profiles_api import models
+from profiles_api import permissions
 
 
 class HelloApiView(APIView):
@@ -73,7 +76,7 @@ class HelloViewSet(viewsets.ViewSet):
 
         if serializer.is_valid():
             name = serializer.validated_data.get('name')
-            message = f'Hello, {name}!'
+            message = f'Hello, {name}! You are added to databse'
             return Response({'message': message})
 
         return Response(
@@ -96,3 +99,11 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self, request, pk=None):
         '''Handle removing an object by its ID'''
         return Response({'http_method': 'DELETE'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    '''Handle creating and updating profiles'''
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
